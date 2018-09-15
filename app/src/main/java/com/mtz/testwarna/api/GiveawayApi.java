defcf47bf08bf4180997984212f6d36933405237
Megan Ayucedar <mgncdr@gmail.com>
2018-09-15 12:06:25 +0700

    nav bar drawer (belum selesai)

--- app/src/main/java/com/mtz/testwarna/api/GiveawayApi.java
+++ app/src/main/java/com/mtz/testwarna/api/GiveawayApi.java
@@ -3,7 +3,10 @@ package com.mtz.testwarna.api;
 import com.mtz.testwarna.value.GiveawayValue;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface GiveawayApi {
@@ -13,5 +16,11 @@ public interface GiveawayApi {
     @GET("giveaways/{user_id}")
     Call<GiveawayValue> getGiveawaysByUser(@Path("user_id") int user_id);
 
-    
+    @POST("giveaways")
+    @FormUrlEncoded
+    Call<GiveawayValue> addGiveaway(@Field("user_id") int user_id,
+                                    @Field("content") String content,
+                                    @Field("image") String image,
+                                    @Field("participants") int participants,
+                                    @Field("status") String status);
 }
